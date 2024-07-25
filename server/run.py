5a74fcedb45bb0b14719a4dfa0c50afe6c5ad848
unknown <mariamzareen184@gmail.com>
2024-07-25 14:08:16 +0530

    new commit

--- server/run.py
+++ server/run.py
@@ -1,4 +1,22 @@
-from server import app
 
-if __name__ =='__main__':
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  
+
+@app.route('/')
+def hello(): 
+    return("hello")
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.get_json()
+    print(data)
+    # Process data as needed
+    return jsonify({"message": "Data received"}), 200
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
